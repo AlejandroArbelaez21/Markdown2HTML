8ba3863be05366b2369a97eb5cd6b3979dcccf31
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-10-22 14:40:16 -0500

    javascript

--- markdown2html.py
+++ markdown2html.py
@@ -7,6 +7,9 @@
 
 import sys
 import os
+import re
+import hashlib
+
 if __name__ == "__main__":
     if len(sys.argv) < 3:
         sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
@@ -14,5 +17,19 @@ if __name__ == "__main__":
     if not os.path.exists(sys.argv[1]):
         sys.stderr.write("Missing " + sys.argv[1] + "\n")
         exit(1)
-    else:
-        exit(0)
+
+    with open(sys.argv[1]) as r:
+        with open(sys.argv[2], 'w') as w:
+            for line in r:
+                length = len(line)
+                headings = line.lstrip('#')
+                heading_count = length - len(headings)
+
+                if 1 <= heading_count <= 6:
+                    line = '<h{}>'.format(
+                        heading_count) + headings.strip() + '</h{}>\n'.format(
+                        heading_count)
+
+                if length > 1:
+                    w.write(line)
+    exit(0)
